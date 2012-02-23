c443bdac8075f7faebcc5844f2f0d12e7f815977
Hylon Perchik <hypothermia.frost@gmail.com>
2012-02-23 21:32:49 +0000

    .

--- settings.js
+++
@@ -1,5 +0,0 @@
-exports.mongourl = 'mongodb://hacker:cgbhn40@ds029847.mongolab.com:29847/data';
-exports.sessionSecret = 'YtGhagthHFTu7*?~6&$|66e4]83^$32730$*87$63';
-exports.facebookSecret = '';
-//application logic
-exports.userRefreshTime = 60000; //ms
